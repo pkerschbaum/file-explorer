d667e29f4538a9309cb18bcddbdddb347e7eb445
Patrick Kerschbaum <patrick.kerschbaum@gmail.com>
2021-10-13 11:41:01 +0200

    chore: open DevTools only in env "development"

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,9 @@
+/* eslint-disable node/no-process-env -- config.ts is the only place where reading from process.env is allowed */
+
+const isDevEnviroment = process.env.NODE_ENV === 'development';
+
 export const config = {
+  isDevEnviroment,
   featureFlags: {
     tags: false,
   },
