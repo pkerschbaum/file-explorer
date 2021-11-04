3b024acd59899b91ae78f9303ff9ed33bdbcbddc
Patrick Kerschbaum <patrick.kerschbaum@gmail.com>
2021-11-04 01:41:25 +0100

    chore: move electron-forge config into separate file

---
+++ forge.config.js
@@ -0,0 +1,44 @@
+module.exports = {
+  packagerConfig: {},
+  makers: [
+    {
+      name: '@electron-forge/maker-squirrel',
+      config: {
+        name: 'electron_react_typescript',
+      },
+    },
+    {
+      name: '@electron-forge/maker-zip',
+      platforms: ['darwin'],
+    },
+    {
+      name: '@electron-forge/maker-deb',
+      config: {},
+    },
+    {
+      name: '@electron-forge/maker-rpm',
+      config: {},
+    },
+  ],
+  plugins: [
+    [
+      '@electron-forge/plugin-webpack',
+      {
+        mainConfig: './webpack.main.config.js',
+        renderer: {
+          config: './webpack.renderer.config.js',
+          entryPoints: [
+            {
+              html: './src/index.html',
+              js: './src/renderer.ts',
+              name: 'main_window',
+              preload: {
+                js: './src/preload.ts',
+              },
+            },
+          ],
+        },
+      },
+    ],
+  ],
+};
