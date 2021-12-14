7c1b7fef1b749a41b4e064e4ccc1df476a2367f3
Patrick Kerschbaum <patrick.kerschbaum@gmail.com>
2021-12-14 12:46:48 +0100

    chore(deps): update all deps

--- src/ui/components-library/Box.tsx
+++ src/ui/components-library/Box.tsx
@@ -3,7 +3,7 @@ import * as React from 'react';
 import styled, { DefaultTheme, StyledComponent } from 'styled-components';
 
 export type BoxProps = React.ComponentProps<'div'> & Pick<MotionProps, 'layout' | 'layoutId'>;
-export const Box: StyledComponent<
+export const Box = styled(motion.div)`` as StyledComponent<
   ForwardRefComponent<HTMLDivElement, BoxProps>,
   DefaultTheme
-> = styled(motion.div)``;
+>;
