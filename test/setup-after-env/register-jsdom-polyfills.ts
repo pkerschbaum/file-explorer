d5ba6e50e6ff319b28481cacaede40ab07dbd5b1
Patrick Kerschbaum <patrick.kerschbaum@gmail.com>
2021-12-27 14:30:38 +0100

    chore: fix jsdom tests

--- test/setup-after-env/register-jsdom-polyfills.ts
+++ test/setup-after-env/register-jsdom-polyfills.ts
@@ -1,6 +1,7 @@
 import 'setimmediate';
 import '@shopify/polyfills/idle-callback.jest';
 import '@testing-library/jest-dom';
+import { functions } from '@app/base/utils/functions.util';
 
 // "polyfill" window.matchMedia (https://jestjs.io/docs/26.x/manual-mocks#mocking-methods-which-are-not-implemented-in-jsdom)
 Object.defineProperty(window, 'matchMedia', {
@@ -16,3 +17,6 @@ Object.defineProperty(window, 'matchMedia', {
     dispatchEvent: jest.fn(),
   })),
 });
+
+// add dummy "scrollIntoView" function (https://github.com/jsdom/jsdom/issues/1695)
+window.HTMLElement.prototype.scrollIntoView = functions.noop;
