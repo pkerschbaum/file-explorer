d03a82ab664f4e5c9c56adddb97b4c82c61d7176
Patrick Kerschbaum <patrick.kerschbaum@gmail.com>
2022-01-04 22:37:22 +0100

    design: remove backdrop of resource rename input
    because of ancestors of this component using "transform", the backdrop gets not always rendered correctly

--- src/ui/resource-rename-input/ResourceRenameInput.tsx
+++ src/ui/resource-rename-input/ResourceRenameInput.tsx
@@ -5,7 +5,7 @@ import styled from 'styled-components';
 import { check } from '@app/base/utils/assert.util';
 import { ResourceForUI } from '@app/domain/types';
 import { commonStyles } from '@app/ui/common-styles';
-import { Backdrop, Button, FocusScope, TextField } from '@app/ui/components-library';
+import { Button, FocusScope, TextField } from '@app/ui/components-library';
 import { KEY } from '@app/ui/constants';
 
 type ResourceRenameInputProps = {
@@ -41,48 +41,38 @@ export const ResourceRenameInput: React.FC<ResourceRenameInputProps> = ({
 
   return (
     <FocusScope contain autoFocus restoreFocus>
-      <RenameBackdrop>
-        <RenameInputForm
-          className={className}
-          onSubmit={(e) => {
-            e.preventDefault();
-            handleSubmit();
-          }}
+      <RenameInputForm
+        className={className}
+        onSubmit={(e) => {
+          e.preventDefault();
+          handleSubmit();
+        }}
+      >
+        <TextField
+          aria-label="new name for resource"
+          value={value}
+          onChange={setValue}
+          onKeyDown={abortOnEsc}
+        />
+        <Button
+          buttonSize="sm"
+          isDisabled={!inputIsValid}
+          /* cannot use type="submit" because of https://github.com/adobe/react-spectrum/issues/1593 */
+          onPress={handleSubmit}
+          onKeyDown={abortOnEsc}
         >
-          <TextField
-            aria-label="new name for resource"
-            value={value}
-            onChange={setValue}
-            onKeyDown={abortOnEsc}
-          />
-          <Button
-            buttonSize="sm"
-            isDisabled={!inputIsValid}
-            /* cannot use type="submit" because of https://github.com/adobe/react-spectrum/issues/1593 */
-            onPress={handleSubmit}
-            onKeyDown={abortOnEsc}
-          >
-            OK
-          </Button>
-          <Button buttonSize="sm" onPress={abortRename} onKeyDown={abortOnEsc}>
-            Abort
-          </Button>
-        </RenameInputForm>
-      </RenameBackdrop>
+          OK
+        </Button>
+        <Button buttonSize="sm" onPress={abortRename} onKeyDown={abortOnEsc}>
+          Abort
+        </Button>
+      </RenameInputForm>
     </FocusScope>
   );
 };
 
-const RenameBackdrop = styled(Backdrop)`
-  width: 100%;
-
-  display: flex;
-  align-items: stretch;
-`;
-
 const RenameInputForm = styled.form`
-  /* relative positioning so that Backdrop is behind the form */
-  position: relative;
+  width: 100%;
   flex-grow: 1;
 
   display: flex;
